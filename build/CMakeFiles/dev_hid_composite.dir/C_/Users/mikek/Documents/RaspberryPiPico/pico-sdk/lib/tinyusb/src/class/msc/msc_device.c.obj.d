eac1cf93da666361243629b912ff6fd6eab46834
Mike Kushnerik <59262898+miekush@users.noreply.github.com>
2022-07-15 18:54:48 -0500

    Delete build directory

--- build/CMakeFiles/dev_hid_composite.dir/C_/Users/mikek/Documents/RaspberryPiPico/pico-sdk/lib/tinyusb/src/class/msc/msc_device.c.obj.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles\dev_hid_composite.dir\C_\Users\mikek\Documents\RaspberryPiPico\pico-sdk\lib\tinyusb\src\class\msc\msc_device.c.obj: \
- C:\Users\mikek\Documents\RaspberryPiPico\pico-sdk\lib\tinyusb\src\class\msc\msc_device.c \
- C:\Users\mikek\Documents\RaspberryPiPico\pico-sdk\lib\tinyusb\src/tusb_option.h \
- C:\Users\mikek\Documents\RaspberryPiPico\pico-sdk\lib\tinyusb\src/common/tusb_compiler.h \
- C:\Users\mikek\Documents\RaspberryPiPico\pico-tinyusb-basic/tusb_config.h \
- C:\Users\mikek\Documents\RaspberryPiPico\pico-sdk\lib\tinyusb\src/common/tusb_mcu.h
